7aabd82b19bc59727ddea01e72dd1eed4fc5c0fa
dongchan1ee <ehdcks3421@gmail.com>
2023-12-01 01:32:35 +0900

    20231201 회원 관리 예제 - 웹 MVC 개발

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String Home() {
+        return "home";
+    }
+
+}
